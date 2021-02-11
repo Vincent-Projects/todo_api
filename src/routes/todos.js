b55a0e524781ea68afbe391e1ca83004a12855bb
Vincent-Projects <crysyroodpro@outlook.com>
2021-02-11 22:51:57 +0100

    Docs v0.1.0 | API v1.1.0

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -10,31 +10,67 @@ const todosRoutes = express.Router();
  */
 
 /**
-* @api {get} /todos Get all user's tasks
+* @api {get} /todos Get user's tasks
 * @apiName GetTodos
 * @apiGroup Todos
 * @apiVersion 0.1.0
 * @apiSampleRequest off
 *
-* @apiSuccess {String} todos        The user' tasks.
+* @apiSuccess {String[]} todos        The user's tasks.
 *
 * @apiError {Object[]}   errors     Contains information about what's wrong for each fields.
 * @apiError {Object}     err        Server Side error & DB errors
 */
 todosRoutes.get('/', todosController.getTodos);
 
+/**
+* @api {post} /todos Add new task
+* @apiName PostTodo
+* @apiGroup Todos
+* @apiVersion 0.1.0
+* @apiSampleRequest off
+*
+* @apiSuccess {String} todos        The user's tasks.
+*
+* @apiError {Object[]}   errors     Contains information about what's wrong for each fields.
+* @apiError {Object}     err        Server Side error & DB errors
+*/
 todosRoutes.post(
     '/add',
     validator.validatePostTask(),
     todosController.postTodo
 );
 
+/**
+* @api {delete} /todos/delete/:todoId Delete task
+* @apiName DeleteTodo
+* @apiGroup Todos
+* @apiVersion 0.1.0
+* @apiSampleRequest off
+*
+* @apiSuccess {String} todos        The user's deleted tasks.
+*
+* @apiError {Object[]}   errors     Contains information about what's wrong for each fields.
+* @apiError {Object}     err        Server Side error & DB errors
+*/
 todosRoutes.delete(
     '/delete/:todoId',
     validator.validateDeleteTaskId(),
     todosController.deleteTodo
 );
 
+/**
+* @api {put} /todos/update Update task
+* @apiName PutUpdate
+* @apiGroup Todos
+* @apiVersion 0.1.0
+* @apiSampleRequest off
+*
+* @apiSuccess {String} todos        The user's updated tasks.
+*
+* @apiError {Object[]}   errors     Contains information about what's wrong for each fields.
+* @apiError {Object}     err        Server Side error & DB errors
+*/
 todosRoutes.put(
     '/update',
     validator.validatePutTodo(),
